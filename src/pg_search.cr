3b95abdd3c7d4e9362dfe69dd7ba17a61486a7d5
dcalixto <calixtodaniel@gmail.com>
2024-12-22 04:09:06 -0300

    set searchable_columns macro

--- src/pg_search.cr
+++ src/pg_search.cr
@@ -1,25 +1,28 @@
 module PgSearch
   macro included
     extend ClassMethods
+
+    macro searchable_columns(columns)
+      def self.search(query : String)
+        return [] of self if query.blank?
+
+        sanitized_query = query.gsub("'", "''")
+        sql = <<-SQL
+          SELECT *, (
+            ts_rank(to_tsvector('english', COALESCE(title, '')), plainto_tsquery('#{sanitized_query}')) +
+            ts_rank(to_tsvector('english', COALESCE(body, '')), plainto_tsquery('#{sanitized_query}'))
+          ) AS search_rank
+          FROM #{table_name}
+          WHERE to_tsvector('english', COALESCE(title, '')) @@ plainto_tsquery('#{sanitized_query}') OR
+                to_tsvector('english', COALESCE(body, '')) @@ plainto_tsquery('#{sanitized_query}')
+          ORDER BY search_rank DESC
+        SQL
+
+        db.query_all(sql, as: self)
+      end
+    end
   end
 
   module ClassMethods
-    def search(query : String)
-      return [] of self if query.blank?
-
-      sanitized_query = query.gsub("'", "''")
-      sql = <<-SQL
-        SELECT *, (
-          ts_rank(to_tsvector('english', COALESCE(title, '')), plainto_tsquery('#{sanitized_query}')) +
-          ts_rank(to_tsvector('english', COALESCE(body, '')), plainto_tsquery('#{sanitized_query}'))
-        ) AS search_rank
-        FROM posts
-        WHERE to_tsvector('english', COALESCE(title, '')) @@ plainto_tsquery('#{sanitized_query}') OR
-              to_tsvector('english', COALESCE(body, '')) @@ plainto_tsquery('#{sanitized_query}'))
-        ORDER BY search_rank DESC
-      SQL
-
-      db.query_all(sql, as: self)
-    end
   end
 end
